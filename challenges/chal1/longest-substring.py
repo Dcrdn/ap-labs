c90ed4d36bda607d2a2e687cd80cc884edabf651
Diego <A01227808@gmail.com>
2019-01-15 20:29:01 -0600

    added input

--- challenges/chal1/longest-substring.py
+++ challenges/chal1/longest-substring.py
@@ -8,5 +8,6 @@ def ls_recursivo(string, sofar, pos):
         return max(ls_recursivo(string,helper,pos-1)+1, ls_recursivo(string,sofar,pos-1))
 
 #word="abcdafg"
+print("Tell me de string: ")
 word=str(input())
 print(ls_recursivo(word,"",len(word)-1))
